6dd372b7e4d6b5e7cf656b0c01df1a7084152213
Sasquatch8946 <42752821+Sasquatch8946@users.noreply.github.com>
2024-12-03 18:38:39 -0600

    remove subscription check in deploy.ps1 for sake of azd

--- iac/deploy.ps1
+++ iac/deploy.ps1
@@ -6,11 +6,6 @@ $kvName = 'kv' + $guidValue
 # if key vault not exist, create
 # else get existing key vault for use in bicep deployment
 $rg = 'azureresume'
-if ((Get-AzContext).Subscription.Id -ne $env:AZURE_SUBSCRIPTION_ID)
-{
-    Write-Error "need to change subscription"
-    Exit 1
-}
 
 $kv = Get-AzKeyVault -ResourceGroupName $rg -VaultName $kvName
 if (-Not($kv))
