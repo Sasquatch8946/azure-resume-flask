823d6b54606f9ef74e32770de7f6c3a906265586
Sasquatch8946 <42752821+Sasquatch8946@users.noreply.github.com>
2024-12-06 07:19:57 -0600

    In deploy_stage_2.ps1, add an if statement that checks for apim named value and deploys the named value and policy if not found.

--- iac/deploy_stage_2.ps1
+++ iac/deploy_stage_2.ps1
@@ -2,7 +2,9 @@ Param
 (
     [string]$guidValue=${env:GUID}
 )
-
+# TODO: need if statements for the named value and probably the policy, too 
+# named value doesn't deploy if key vault secret isn't created
+# Get-AzApiManagementNamedValue
 Write-Host "GUIDVALUE: $guidValue"
 $kvName = 'kv' + $guidValue
 $rg = 'azureresume'
@@ -13,6 +15,18 @@ if (-Not($apim))
     Write-Host "API management service not found. Deploying..."
     $deploy = New-AzResourceGroupDeployment -ResourceGroupName $rg -TemplateFile "$PSScriptRoot/apim.bicep" -TemplateParameterObject @{"guidValue"=$guidValue}
     $deploy.Outputs
+}
+else  
+{
+    Write-Host "API management service already exists."
+}
+
+# Get-AzApiManagementNamedValue
+$apimContext = New-AzApiManagementContext -ResourceGroupName $rg -ServiceName "apim$guidValue"
+$namedValue = Get-AzApiManagementNamedValue -Context $apimContext -Name "azresume-keyvault-ref"
+if (-Not($namedValue))
+{
+    Write-Host "API management named value not found. Deploying named value and policy..."
     $deploy2 = New-AzResourceGroupDeployment -ResourceGroupName $rg -TemplateFile "$PSScriptRoot/namedvalue.bicep" -TemplateParameterObject @{"guidValue"=$guidValue}
     $deploy2.Outputs
     $deploy3 = New-AzResourceGroupDeployment -ResourceGroupName $rg -TemplateFile "$PSScriptRoot/apimPolicy.bicep" -TemplateParameterObject @{"guidValue"=$guidValue}
@@ -23,4 +37,3 @@ else
 {
     Write-Host "API management service already exists."
 }
-
