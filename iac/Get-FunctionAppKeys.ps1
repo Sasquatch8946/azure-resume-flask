f82ef7ccf9f369524697f07a92ab4ec6640a23a4
Sasquatch8946 <42752821+Sasquatch8946@users.noreply.github.com>
2025-01-13 06:36:45 -0600

    parameterize guidvalue and resourcegroup in get-functionappkeys.ps1

--- iac/Get-FunctionAppKeys.ps1
+++ iac/Get-FunctionAppKeys.ps1
@@ -1,6 +1,9 @@
-# This script gets azure function function key and stores it in keyvault for the API Management service to use later.
-$guidValue = ${env:GUID}
-$rg = ${env:resourceGroup}
-$secret = az functionapp keys list -n "crcfunc$guidValue" -g $rg | ConvertFrom-Json | select -expand  functionKeys | select -expand default
+Param
+(
+    [string]$guidValue = ${env:GUID},
+    [string]$rg = ${env:resourceGroup}
+)
 
+# This script gets azure function function key and stores it in keyvault for the API Management service to use later.
+$secret = az functionapp keys list -n "crcfunc$guidValue" -g "$rg" | ConvertFrom-Json | select -expand  functionKeys | select -expand default
 az keyvault secret set --vault-name "kv$guidValue" --name "azresume-func-key" --value $secret
