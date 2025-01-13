125313c84fa4f6805cfda3ae58d8660ff03cb150
Sasquatch8946 <42752821+Sasquatch8946@users.noreply.github.com>
2025-01-13 12:27:28 -0600

    update .gitignore and requirements.txt

---
+++ app_svc/tests/test_app.py
@@ -0,0 +1,38 @@
+import sys
+import os 
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from app import app 
+import pytest
+import requests_mock
+
+@pytest.fixture
+def mock_api():
+    with requests_mock.Mocker() as m:
+        yield m
+
+@pytest.fixture
+def client():
+    """A test client for the app."""
+    with app.test_client() as client:
+        yield client
+
+def test_home(client):
+    """Test the home route."""
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Hi, I'm Sean Chapman" in response.data
+
+def test_about(client, mock_api):
+    """Test the about route."""
+    response = mock_api.get('/read_db', json={"id": "1", "count": 2})
+    print(response)
+    # assert response.status_code == 200
+    assert response["count"] == 2
+
+
+def test_non_existent_route(client):
+    """Test for a non-existent route."""
+    response = client.get('/non-existent')
+    assert response.status_code == 404
