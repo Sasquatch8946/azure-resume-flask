d11ecc489e7dc2465dc86688b72ad88f0e08b88a
Sasquatch8946 <42752821+Sasquatch8946@users.noreply.github.com>
2025-01-13 06:20:19 -0600

    have get-functionappkeys.ps1 use env var for resource group

--- iac/Get-FunctionAppKeys.ps1
+++ iac/Get-FunctionAppKeys.ps1
@@ -1,6 +1,6 @@
 # This script gets azure function function key and stores it in keyvault for the API Management service to use later.
 $guidValue = ${env:GUID}
-
-$secret = az functionapp keys list -n "crcfunc$guidValue" -g 'azureresume' | ConvertFrom-Json | select -expand  functionKeys | select -expand default
+$rg = ${env:resourceGroup}
+$secret = az functionapp keys list -n "crcfunc$guidValue" -g $rg | ConvertFrom-Json | select -expand  functionKeys | select -expand default
 
 az keyvault secret set --vault-name "kv$guidValue" --name "azresume-func-key" --value $secret
